c6492a41ec2bbbd6d1d2c0c4d3ef06d70e2f602d
sametdemirezen <sametdemirezen16@gmail.com>
2023-09-28 17:30:37 +0200

    checklist

--- ourWinch/Controllers/ServiceOrderController.cs
+++ ourWinch/Controllers/ServiceOrderController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ServiceOrderController : Controller
 {
-    public IActionResult Index()
+    public IActionResult Checklist()
     {
         return View();
     }
