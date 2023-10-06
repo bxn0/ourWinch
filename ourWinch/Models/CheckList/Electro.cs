ab7fceece1701d61c4f42f50122ea2edb92537ac
Umit <umity@uia.no>
2023-10-06 13:30:27 +0200

    checklist model

---
+++ ourWinch/Models/CheckList/Electro.cs
@@ -0,0 +1,9 @@
+﻿// CheckList altında Electro modeli
+namespace ourWinchSist.Models.CheckList
+{
+    public class Electro
+    {
+        public int Id { get; set; }
+        // Diğer özellikleri buraya ekleyin
+    }
+}
---
+++ ourWinch/Models/CheckList/FunctionTest.cs
@@ -0,0 +1,9 @@
+﻿// CheckList altında FunctionTest modeli
+namespace ourWinchSist.Models.CheckList
+{
+    public class FunctionTest
+    {
+        public int Id { get; set; }
+        // Diğer özellikleri buraya ekleyin
+    }
+}
