7105b2519fa59a9bc4bf80ba36d51c3047f7c763
Umit <umity@uia.no>
2023-10-15 21:33:31 +0200

    electro

---
+++ ourWinch/Controllers/Checklist/HydroliskController.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ourWinch.Controllers.Checklist
+{
+    public class HydroliskController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HydroliskController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Mechanical
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Mechanicals.ToListAsync());
+        }
+
+
+        // GET: Mechanical/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mechanical = await _context.Mechanicals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mechanical == null)
+            {
+                return NotFound();
+            }
+
+            return View(mechanical);
+        }
+
+        // GET: Mechanical/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mechanical/Create
+        // POST: Mechanical/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MechanicalListViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isFirst = true;
+
+                // En son eklenen ServiceOrder'ı alıyoruz.
+                var lastServiceOrder = _context.ServiceOrders.OrderByDescending(o => o.ServiceOrderId).FirstOrDefault();
+
+                if (lastServiceOrder != null)
+                {
+                    foreach (var mechanical in viewModel.Mechanicals)
+                    {
+                        if (isFirst)
+                        {
+                            mechanical.Kommentar = viewModel.Kommentar;
+                            isFirst = false;
+                        }
+
+                        // Her bir Mechanical için ServiceOrder'dan Ordrenummer'ı alıyoruz.
+                        mechanical.Ordrenummer = lastServiceOrder.Ordrenummer;
+                        mechanical.ServiceOrderId = lastServiceOrder.ServiceOrderId;
+
+                        _context.Add(mechanical);
+                    }
+                    await _context.SaveChangesAsync();
+                    return Redirect("/ServiceOrder/Details/2");
+                }
+                else
+                {
+                    // Eğer hiç ServiceOrder bulunamazsa bir hata mesajı döndürebilirsiniz.
+                    ModelState.AddModelError(string.Empty, "ServiceOrder bulunamadı.");
+                }
+            }
+            return View(viewModel);
+        }
+
+
+        // GET: Mechanical/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mechanical = await _context.Mechanicals.FindAsync(id);
+            if (mechanical == null)
+            {
+                return NotFound();
+            }
+            return View(mechanical);
+        }
+
+        // POST: Mechanical/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ChecklistItem,OK,BorSkiftes,Defekt")] Mechanical mechanical)
+        {
+            if (id != mechanical.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mechanical);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MechanicalExists(mechanical.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mechanical);
+        }
+
+        // GET: Mechanical/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mechanical = await _context.Mechanicals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mechanical == null)
+            {
+                return NotFound();
+            }
+
+            return View(mechanical);
+        }
+
+        // POST: Mechanical/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mechanical = await _context.Mechanicals.FindAsync(id);
+            _context.Mechanicals.Remove(mechanical);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MechanicalExists(int id)
+        {
+            return _context.Mechanicals.Any(e => e.Id == id);
+        }
+    }
+}
