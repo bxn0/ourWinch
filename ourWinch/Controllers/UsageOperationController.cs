9206efda57529ef5c9b611af193e443cf6d83f1c
aslnthir <aslnthir@gmail.com>
2023-10-17 16:11:35 +0200

    usage model added

--- ourWinch/Controllers/UsageOperationController.cs
+++ ourWinch/Controllers/UsageOperationController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using ourWinch.Models; // Models namespace'ini ekleyin
 namespace ourWinch.Controllers
 {
     public class UsageOperationsController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            var model = new RegistrationModel(); // Modelin yeni bir örneğini oluştur
+            return View(model);
         }
     }
 }
