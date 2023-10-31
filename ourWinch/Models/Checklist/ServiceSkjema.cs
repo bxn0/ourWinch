79c6de9eac6bdf3ef47ee0f62daa9dc6b7ae4df4
Umit <umity@uia.no>
2023-10-31 02:27:40 +0100

    API add

---
+++ ourWinch/Models/Checklist/ServiceSkjema.cs
@@ -0,0 +1,17 @@
+﻿using ourWinch.Models.Dashboard;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ServiceSkjema
+{
+
+    public List<Mechanical> Mechanicals { get; set; }
+    public List<Hydrolisk> Hydrolisks { get; set; }
+    public List<Electro> Electros { get; set; }
+    public List<FunksjonsTest> FunksjonsTests { get; set; }
+    public List<Trykk> Trykks { get; set; }
+ 
+}
+
+
+
+
