46c80c84bed00f7b7d82e79a2e6e4d18879cd930
Orhanyil <yildirimorhan4790@gmail.com>
2023-11-01 21:59:13 +0100

    view kunde info added

---
+++ ourWinch/Controllers/Dashboard/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using ourWinch.Models.Dashboard;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+
+
+
+
+[Authorize]
+public class CustomersController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public CustomersController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Dashboard()
+    {
+        List<DateTime> bookedDates = _context.ServiceOrders.Select(s => s.MottattDato).ToList();
+        ViewBag.BookedDates = Newtonsoft.Json.JsonConvert.SerializeObject(bookedDates);
+        return View("~/Views/Dashboard/Customers.cshtml");
+    }
+}
