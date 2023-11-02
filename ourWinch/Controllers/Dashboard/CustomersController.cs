156b5d88ec885609fd7080ed0da72f70aa5f79b4
Orhanyil <yildirimorhan4790@gmail.com>
2023-11-02 11:19:59 +0100

    Customers unik list added

--- ourWinch/Controllers/Dashboard/CustomersController.cs
+++ ourWinch/Controllers/Dashboard/CustomersController.cs
@@ -4,9 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using Microsoft.AspNetCore.Authorization;
 using Newtonsoft.Json;
-
-
-
+using System.Linq;
 
 [Authorize]
 public class CustomersController : Controller
@@ -21,7 +19,14 @@ public class CustomersController : Controller
     public IActionResult Dashboard()
     {
         List<DateTime> bookedDates = _context.ServiceOrders.Select(s => s.MottattDato).ToList();
-        ViewBag.BookedDates = Newtonsoft.Json.JsonConvert.SerializeObject(bookedDates);
-        return View("~/Views/Dashboard/Customers.cshtml");
+        ViewBag.BookedDates = JsonConvert.SerializeObject(bookedDates);
+
+        // Benzersiz "navn", "Etter Navn" ve "Tlf Nummer" değerlerine sahip satırları al
+        var uniqueServiceOrders = _context.ServiceOrders
+            .GroupBy(s => new { s.Fornavn, s.Etternavn, s.MobilNo })
+            .Select(g => g.First())
+            .ToList();
+
+        return View("~/Views/Dashboard/Customers.cshtml", uniqueServiceOrders);
     }
 }
