03e462106eaf46550f339ced5a303a9e54a2bd0b
Burak Seymen <82652466+bxn0@users.noreply.github.com>
2023-11-04 13:02:19 +0100

    unit tests added for; Login, LoginView, Register, RegisterView, Logout

---
+++ ourWinch.Tests/AccountController/RegisterView.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ourWinch.Models.Account;
+
+
+public class RegisterView
+{
+    // Mocks for the UserManager, SignInManager, and RoleManager are created
+    private readonly Mock<UserManager<ApplicationUser>> _userManagerMock = MockUserManager();
+    private readonly Mock<SignInManager<ApplicationUser>> _signInManagerMock = MockSignInManager();
+    private readonly Mock<RoleManager<IdentityRole>> _roleManagerMock = MockRoleManager();
+
+    public RegisterView()
+    {
+        // Use 'this' to reference the current instance's fields
+        _userManagerMock = new Mock<UserManager<ApplicationUser>>(
+            Mock.Of<IUserStore<ApplicationUser>>(),
+            null, null, null, null, null, null, null, null);
+
+        // You need to create signInManagerMock here instead of using field initializer
+        _signInManagerMock = new Mock<SignInManager<ApplicationUser>>(
+            _userManagerMock.Object,
+            Mock.Of<IHttpContextAccessor>(),
+            Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(),
+            null, null, null, null);
+
+        _roleManagerMock = new Mock<RoleManager<IdentityRole>>(
+            Mock.Of<IRoleStore<IdentityRole>>(),
+            null, null, null, null);
+    }
+    [Fact]
+    public async Task Register_Post_ValidModel_CreatesUserAndReturnsRedirect()
+    {
+        // Arrange
+        var model = new RegisterViewModel
+        {
+            // Populate the model with valid data required for registration
+        };
+        // Setup the UserManager to return a successful result when CreateAsync is called
+        _userManagerMock.Setup(um => um.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        // Instantiate the controller with the mocked dependencies
+        var controller = new AccountController(_userManagerMock.Object, _signInManagerMock.Object, _roleManagerMock.Object);
+
+        // Act
+        var result = await controller.Register(model);
+
+        // Assert
+        // The result should be a redirect to the Dashboard's Index action
+        result.Should().BeOfType<RedirectToActionResult>();
+        var redirectToActionResult = result as RedirectToActionResult;
+        redirectToActionResult.ControllerName.Should().Be("Dashboard");
+        redirectToActionResult.ActionName.Should().Be("Index");
+        // Verify that CreateUser was called once
+        _userManagerMock.Verify(um => um.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()), Times.Once);
+    }
+
+    // More tests go here, following the same structure as the one above
+
+    // Helper method to create a mock UserManager
+    private static Mock<UserManager<ApplicationUser>> MockUserManager()
+    {
+        var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+        return new Mock<UserManager<ApplicationUser>>(
+            userStoreMock.Object, null, null, null, null, null, null, null, null);
+    }
+
+    // Helper method to create a mock SignInManager
+    private static Mock<SignInManager<ApplicationUser>> MockSignInManager()
+    {
+        var contextAccessorMock = new Mock<IHttpContextAccessor>();
+        var userPrincipalFactoryMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+        return new Mock<SignInManager<ApplicationUser>>();
+
+    }
+
+    // Helper method to create a mock RoleManager
+    private static Mock<RoleManager<IdentityRole>> MockRoleManager()
+    {
+        var roleStore = new Mock<IRoleStore<IdentityRole>>();
+        return new Mock<RoleManager<IdentityRole>>(
+            roleStore.Object, null, null, null, null);
+    }
+}
---
+++ ourWinch.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ourWinch.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
