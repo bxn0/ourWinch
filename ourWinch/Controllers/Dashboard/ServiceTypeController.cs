184a59305167f4426df2aa797037c410d9b7839d
Orhanyil <yildirimorhan4790@gmail.com>
2023-11-05 15:32:59 +0100

    Rapporter.Tjenestetype: data fetched from serviceOrder

--- ourWinch/Controllers/Dashboard/ServiceTypeController.cs
+++ ourWinch/Controllers/Dashboard/ServiceTypeController.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using ourWinch.Models.Dashboard;
+using System.Linq;
+
 namespace ourWinch.Controllers
 {
     public class ServiceTypeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        // AppDbContext sınıfınızı dependency injection ile enjekte edin.
+        public ServiceTypeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Dashboard()
         {
-            // Statik verileri ViewBag ile View'a gönder
-            ViewBag.GarantiCount = 125;
-            ViewBag.ServiceCount = 88;
-            ViewBag.ReperasjonCount = 245;
+            // Veritabanından verileri çek
+            ViewBag.GarantiCount = _context.ServiceOrders.Count(so => so.Garanti);
+            ViewBag.ServiceCount = _context.ServiceOrders.Count(so => so.Servis);
+            ViewBag.ReperasjonCount = _context.ServiceOrders.Count(so => so.Reperasjon);
 
             return View("~/Views/Dashboard/ServiceType.cshtml");
         }
