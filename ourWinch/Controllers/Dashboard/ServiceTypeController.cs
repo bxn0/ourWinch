5dd1b077aae416fbd5947d9d453ed29b94055ac9
Orhanyil <yildirimorhan4790@gmail.com>
2023-11-05 14:57:02 +0100

    view: ServisType addet and controllers: ServisTypeController added

---
+++ ourWinch/Controllers/Dashboard/ServiceTypeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ourWinch.Controllers
+{
+    public class ServiceTypeController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            // Statik verileri ViewBag ile View'a gönder
+            ViewBag.GarantiCount = 125;
+            ViewBag.ServiceCount = 88;
+            ViewBag.ReperasjonCount = 245;
+
+            return View("~/Views/Dashboard/ServiceType.cshtml");
+        }
+    }
+}
