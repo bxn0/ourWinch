a4c6049064435cd688e8d3da6acb4502a7214959
Burak Seymen <82652466+bxn0@users.noreply.github.com>
2023-11-15 13:00:23 +0100

    role controller added

---
+++ ourWinch/Controllers/Account/RolesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ourWinch.Models.Account;
+
+namespace ourWinch.Controllers.Account
+{
+    public class RolesController : Controller
+    {
+
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+
+        public RolesController(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+
+            return View(roles);
+        }
+    }
+}
