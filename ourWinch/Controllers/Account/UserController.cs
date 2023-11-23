fb9f74f9ef0732c073b089d1e7840c4525d4e644
Burak Seymen <82652466+bxn0@users.noreply.github.com>
2023-11-23 23:13:32 +0100

    doc added

--- ourWinch/Controllers/Account/UserController.cs
+++ ourWinch/Controllers/Account/UserController.cs
@@ -74,44 +74,94 @@ namespace ourWinch.Controllers.Account
             return View(userList);
         }
 
+
+        /// <summary>
+        /// Displays the edit view for a specific user based on their ID.
+        /// If the user is not found, returns a 'NotFound' response.
+        /// The method also prepares and assigns a list of roles for display in a dropdown in the view.
+        /// </summary>
+        /// <param name="userId">The ID of the user to be edited.</param>
+        /// <returns>
+        /// The 'Edit' view populated with the user's details and a list of roles.
+        /// Returns 'NotFound' if the user does not exist.
+        /// </returns>
         public IActionResult Edit(string userId)
         {
+
+            // Attempt to find the user in the database based on the provided userId.
             var objFromDB = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+
+            // If no user is found with the provided ID, return a NotFound result (404 error).
             if (objFromDB == null)
             {
                 return NotFound();
             }
+
+            // Retrieve all associations of users with roles.
             var userRole = _db.UserRoles.ToList();
+
+            // Retrieve all role definitions.
             var roles = _db.Roles.ToList();
+
+            // Find the role associated with the user, if any.
             var role = userRole.FirstOrDefault(u => u.UserId == userId);
+
+
+            // If the user has a role assigned, set the RoleId in the objFromDB to the corresponding role's ID.
             if (role != null)
             {
                 objFromDB.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
             }
 
+
+            // Prepare a list of roles in the format required for a dropdown list in the view.
+            // Each role is represented as a SelectListItem with its Text and Value set
             objFromDB.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
             {
                 Text = u.Name,
                 Value = u.Id,
             });
-
+            // Return the 'Edit' view, providing the user's details including their role and the list of roles for a dropdown.
             return View(objFromDB);
         }
 
+
+
+
+        /// <summary>
+        /// Processes the submission for editing a user's details and role.
+        /// If the user is not found, a 'NotFound' response is returned.
+        /// The method updates the user's role and other details in the database.
+        /// </summary>
+        /// <param name="user">The user object containing the updated details.</param>
+        /// <returns>
+        /// A redirection to the index view on successful edit,
+        /// or the edit view with the updated user data if the model state is not valid.
+        /// </returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(ApplicationUser user)
         {
+
+            // Validate the model state.
             if (ModelState.IsValid)
             {
+
+                // Retrieve the user from the database based on the provided user ID.
                 var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == user.Id);
+
+                // If no user is found, return a NotFound result.
                 if (objFromDb == null)
                 {
                     return NotFound();
                 }
+
+                // Check if the user has an existing role.
                 var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == objFromDb.Id);
                 if (userRole != null)
                 {
+
+                    // Retrieve the name of the user's current role.
                     var previousRoleName = _db.Roles.Where(u => u.Id == userRole.RoleId).Select(e => e.Name).FirstOrDefault();
                     //removing the old role
                     await _userManager.RemoveFromRoleAsync(objFromDb, previousRoleName);
@@ -120,18 +170,26 @@ namespace ourWinch.Controllers.Account
 
                 //add new role
                 await _userManager.AddToRoleAsync(objFromDb, _db.Roles.FirstOrDefault(u => u.Id == user.RoleId).Name);
+
+                // Update the user's details in the database.
                 objFromDb.Fornavn = user.Fornavn;
+
+                //save changes
                 _db.SaveChanges();
+
+                // Notify success and redirect to the index view.
                 _irisService.Success("Brukeren ble redigert!",3);
                 return RedirectToAction(nameof(Index));
             }
 
-
+            // Prepare the role list for the dropdown if the model state is not valid.
             user.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
             {
                 Text = u.Name,
                 Value = u.Id
             });
+
+            // Return the view with the user data for further editing.
             return View(user);
         }
 
