ea0200256b3861996375dcee633d62ee09c8e4a0
Umit <umity@uia.no>
2023-11-23 21:25:47 +0100

    Electro param

--- ourWinch/Controllers/Checklist/ElectroController.cs
+++ ourWinch/Controllers/Checklist/ElectroController.cs
@@ -175,59 +175,103 @@ namespace ourWinch.Controllers.Checklist
                 {
                     foreach (var electro in viewModel.Electros)
                     {
+                        // If this is the first iteration, copy the comment from the view model.
                         if (isFirst)
                         {
                             electro.Kommentar = viewModel.Kommentar;
                             isFirst = false;
                         }
 
+                        // Set the order number and service order ID for each electro item.
+
                         electro.Ordrenummer = lastServiceOrder.Ordrenummer;
                         electro.ServiceOrderId = lastServiceOrder.ServiceOrderId;
 
+                        // Add the new electro item to the context.
                         _context.Add(electro);
                     }
+                    // Save all changes to the database.
                     await _context.SaveChangesAsync();
+
+                    // Display a success message.
                     _irisService.Success("Listen ble lagret med suksess.",3);
+
+                    // Update the service schema status.
                     await UpdateServicejemaIfAllCompleted(serviceOrderId);
 
+                    // Redirect to the Create action of the FunksjonsTest controller with the appropriate parameters.
                     return RedirectToAction("Create", "FunksjonsTest", new { serviceOrderId = viewModel.ServiceOrderId, category = "FunksjonsTest" });
                 }
                 else
                 {
+                    // If no last service order is found, add a model error.
                     ModelState.AddModelError(string.Empty, "ServiceOrder bulunamadı.");
                 }
             }
             else
             {
+                // If the model state is invalid, collect the error messages to display.
+
                 ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
             }
+            // Return the view with the provided view model, including any errors.
             return View(viewModel);
         }
 
+        /// <summary>
+        /// Asynchronously gets an Electro entity for editing.
+        /// </summary>
+        /// <param name="id">The ID of the Electro entity to be edited. Nullable.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The task result contains the IActionResult for the Edit view.
+        /// If the id is null or an Electro entity with the specified id is not found, NotFound (HTTP 404) is returned.
+        /// Otherwise, the Edit view for the Electro entity is displayed.
+        /// </returns>
         public async Task<IActionResult> Edit(int? id)
         {
+            // If no ID is provided, return a NotFound result.
             if (id == null)
             {
                 return NotFound();
             }
 
+            // Asynchronously find the Electro entity by the given ID.
             var electro = await _context.Electros.FindAsync(id);
+
+            // If no Electro entity is found, return NotFound.
             if (electro == null)
             {
                 return NotFound();
             }
+            // If an Electro entity is found, pass it to the Edit view and return the view result.
             return View(electro);
         }
+
+        /// <summary>
+        /// Asynchronously processes the editing of an existing Electro entity.
+        /// </summary>
+        /// <param name="id">The ID of the Electro entity being edited.</param>
+        /// <param name="electro">The Electro entity that has been bound from the edit form. Properties bound are 'Id', 'ChecklistItem', 'OK', 'BorSkiftes', and 'Defekt'.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The task result contains an IActionResult.
+        /// If the id does not match the entity's ID, NotFound (HTTP 404) is returned.
+        /// If the model state is valid, the changes are saved and redirects to the Index action.
+        /// If there is a concurrency error and the Electro entity no longer exists, NotFound is returned.
+        /// Otherwise, the Edit view is re-displayed with the current Electro entity.
+        /// </returns>
+        /// <exception cref="DbUpdateConcurrencyException">Thrown when a concurrency error is encountered while saving the Electro entity.</exception>
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,ChecklistItem,OK,BorSkiftes,Defekt")] Electro electro)
         {
+            // Check if the ID in the URL matches the entity's ID.
             if (id != electro.Id)
             {
                 return NotFound();
             }
 
+            // If the model state is valid, attempt to update and save the entity.
             if (ModelState.IsValid)
             {
                 try
@@ -237,36 +281,67 @@ namespace ourWinch.Controllers.Checklist
                 }
                 catch (DbUpdateConcurrencyException)
                 {
+                    // Check if the Electro entity still exists.
                     if (!ElectroExists(electro.Id))
                     {
                         return NotFound();
                     }
                     else
                     {
+                        // Re-throw the concurrency exception if the Electro entity exists.
+
                         throw;
                     }
                 }
+                // If successful, redirect to the Index action.
                 return RedirectToAction(nameof(Index));
             }
+
+            // If the model state is not valid, re-display the Edit view with the current Electro entity.
             return View(electro);
         }
 
+        /// <summary>
+        /// Asynchronously retrieves an Electro entity to confirm deletion.
+        /// </summary>
+        /// <param name="id">The ID of the Electro entity to delete. Nullable.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The task result contains the IActionResult for the Delete confirmation view.
+        /// If the id is null or an Electro entity with the specified id is not found, NotFound (HTTP 404) is returned.
+        /// Otherwise, the Delete confirmation view for the Electro entity is displayed.
+        /// </returns>
         public async Task<IActionResult> Delete(int? id)
         {
+            // Validate the ID is not null.
             if (id == null)
             {
                 return NotFound();
             }
 
+            // Retrieve the Electro entity by the provided ID asynchronously.
             var electro = await _context.Electros.FirstOrDefaultAsync(m => m.Id == id);
+
+            // If the Electro entity is not found, return NotFound.
             if (electro == null)
             {
                 return NotFound();
             }
 
+            // If an Electro entity is found, pass it to the Delete confirmation view and return the view result.
             return View(electro);
         }
 
+        /// <summary>
+        /// Asynchronously deletes the Electro entity with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID of the Electro entity to delete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous delete operation and navigation to the Index action upon completion.
+        /// </returns>
+        /// <remarks>
+        /// This is a POST action protected against Cross-Site Request Forgery (CSRF).
+        /// The entity is first retrieved and then removed from the database context followed by saving the changes.
+        /// </remarks>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -277,11 +352,21 @@ namespace ourWinch.Controllers.Checklist
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// Checks if an Electro entity with the specified ID exists in the database.
+        /// </summary>
+        /// <param name="id">The ID of the Electro entity to check.</param>
+        /// <returns>True if the Electro entity exists; otherwise, false.</returns>
         private bool ElectroExists(int id)
         {
             return _context.Electros.Any(e => e.Id == id);
         }
 
+        /// <summary>
+        /// Asynchronously updates the service schema to reflect completion if all associated tasks are completed.
+        /// </summary>
+        /// <param name="serviceOrderId">The ID of the service order to check for completion.</param>
+        /// <returns>A task that represents the asynchronous update operation.</returns>
         private async Task UpdateServicejemaIfAllCompleted(int serviceOrderId)
         {
             await _serviceSkjemaService.UpdateServicejemaIfAllCompleted(serviceOrderId);
