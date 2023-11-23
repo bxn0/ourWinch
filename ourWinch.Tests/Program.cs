e628e92e764216ddf0dd00285722f20b2169c822
Bunyamin54 <bunyaming@uia.no>
2023-11-23 09:27:27 +0100

    Names, surnames and github e-mails of those who contributed to the project were added. Additionally, which programming languages ​​were used in the project was added.

---
+++ ourWinch.Tests/Program.cs
@@ -0,0 +1,123 @@
+﻿using AspNetCoreHero.ToastNotification;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using ourWinch.Models.Account;
+using ourWinch.Services;
+
+
+namespace ourWinch
+{
+    public class Program
+    {
+
+        
+        public static void Main(string[] args)
+        {
+
+            
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+
+            // App Configuration
+            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string is missing.");
+            }
+
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            //builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConsole();
+                loggingBuilder.AddDebug();
+            });
+            builder.Services.AddTransient<IEmailSender, MailJetEmailSender>();
+            builder.Services.Configure<IdentityOptions>(opt =>
+            {
+                opt.Password.RequiredLength = 5;
+                opt.Password.RequireLowercase = true;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(15);
+                opt.Lockout.MaxFailedAccessAttempts = 6;
+
+            });
+
+
+
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddNotyf(config =>
+            {
+                config.DurationInSeconds = 10;
+                config.IsDismissable = true;
+                config.Position = NotyfPosition.TopCenter;
+            });
+
+            // Add the DbContext to the DI container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.CommandTimeout(30); // Veritabanı komutlarının zaman aşımı süresini ayarla
+                })
+                .LogTo(Console.WriteLine); // Bu satırı .UseSqlServer çağrısından sonra ve ayrı bir satırda ekle
+            });
+
+            // Register ServiceSkjemaService for DI.
+            builder.Services.AddScoped<ServiceSkjemaService>();
+
+            // Set the server to listen on 0.0.0.0:5002
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.ListenAnyIP(5002);
+                Console.WriteLine("Kestrel is now configured to listen on port 5002 for any IP address.");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name v1"));
+                app.UseDeveloperExceptionPage();  // Bu satırı ekledik.
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Account}/{action=Login}/{id?}");
+
+            app.Run();
+        }
+    }
+}
