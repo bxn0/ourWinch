e79092d63e22b31b37b109d064907980193b8f01
Umit <umity@uia.no>
2023-11-23 17:55:02 +0100

    Param Checklist

--- ourWinch/Controllers/Checklist/ElectroController.cs
+++ ourWinch/Controllers/Checklist/ElectroController.cs
@@ -8,14 +8,34 @@ using AspNetCoreHero.ToastNotification.Abstractions;
 
 namespace ourWinch.Controllers.Checklist
 {
+    /// <summary>
+    /// The ElectroController handles all the electro-related operations.
+    /// It requires authorization to access its methods.
+    /// </summary>
     [Authorize]
     public class ElectroController : Controller
     {
+        /// <summary>
+        /// The database context used for data operations related to electro.
+        /// </summary>
         private readonly AppDbContext _context;
+        /// <summary>
+        /// Service for managing service forms specific to electro operations.
+        /// </summary>
         private readonly ServiceSkjemaService _serviceSkjemaService;
+        /// <summary>
+        /// Notification service to communicate with users or systems, named Iris for project-specific reasons.
+        /// </summary>
         private readonly INotyfService _irisService;
 
-        // Dependency Injection ile AppDbContext ve ServiceSkjemaService ekleniyor
+        // Constructors, methods, and other members would follow with similar documentation.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ElectroController"/> class.
+        /// </summary>
+        /// <param name="context">The database context to be used for data operations.</param>
+        /// <param name="serviceSkjemaService">The service for handling service schematics.</param>
+        /// <param name="irisService">The notification service for user communication.</param>
         public ElectroController(AppDbContext context, ServiceSkjemaService serviceSkjemaService, INotyfService irisService)
         {
             _context = context;
@@ -23,26 +43,48 @@ namespace ourWinch.Controllers.Checklist
             _irisService = irisService;
         }
 
+        /// <summary>
+        /// Asynchronously gets the list of Mechanicals from the database context and returns the Index view.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the IActionResult of the Index view.</returns>
+
         public async Task<IActionResult> Index()
         {
+            // Retrieves all entries for Mechanicals from the database and converts them to a list asynchronously.
             return View(await _context.Mechanicals.ToListAsync());
         }
 
+        /// <summary>
+        /// Asynchronously retrieves and displays the details of a specific electro item.
+        /// </summary>
+        /// <param name="id">The ID of the electro item to find. Nullable.</param>
+        /// <returns>
+        /// If the id is null or an electro item with the specified id is not found, NotFound (HTTP 404) is returned.
+        /// Otherwise, the Details view for the electro item is returned.
+        /// </returns>
         public async Task<IActionResult> Details(int? id)
         {
+            // Check if the id parameter is null.
             if (id == null)
             {
                 return NotFound();
             }
 
+            // Retrieve the electro item by the provided ID asynchronously.
             var electro = await _context.Electros.FirstOrDefaultAsync(m => m.Id == id);
+
+            // If an electro item with the provided ID was not found, return NotFound (HTTP 404).
             if (electro == null)
             {
                 return NotFound();
             }
 
+            // If an electro item is found, pass it to the Details view and return the view result.
+
             return View(electro);
         }
+
+
         [HttpGet]
         [Route("Electro/Create/{serviceOrderId}/{category}")]
         public IActionResult Create(int serviceOrderId, string category = "Electro")
