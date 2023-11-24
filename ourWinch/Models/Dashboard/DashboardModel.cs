b43357bf7c5beca881a0a2275ca34511b7f0cfb7
Orhanyil <yildirimorhan4790@gmail.com>
2023-11-24 09:39:54 +0100

    Documentation of Model-Dashboard-DashboardModel.cs is completed.

--- ourWinch/Models/Dashboard/DashboardModel.cs
+++ ourWinch/Models/Dashboard/DashboardModel.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections.Generic;  
+﻿using System.Collections.Generic;
 
 namespace ourWinch.Models.Dashboard
 {
+    /// <summary>
+    /// Represents the model for a dashboard view, containing title and side menu items.
+    /// </summary>
     public class DashboardModel
     {
+        /// <summary>
+        /// Gets or sets the title of the dashboard.
+        /// </summary>
         public string? Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the items to be displayed in the side menu of the dashboard.
+        /// </summary>
         public List<string> SideMenuItems { get; set; } = new List<string>();
     }
 }
+
